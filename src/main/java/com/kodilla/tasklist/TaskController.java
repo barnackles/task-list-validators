e5c7d3b575f13c1c905cd4d07d9157ee25ceb565
barnackles <p.martewicz@yahoo.com>
2023-05-29 17:43:15 +0200

    Validators added.

--- src/main/java/com/kodilla/tasklist/TaskController.java
+++ src/main/java/com/kodilla/tasklist/TaskController.java
@@ -1,17 +1,21 @@
 package com.kodilla.tasklist;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+@Validated
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
     @GetMapping("/{priority}")
-    public ResponseEntity<List<TaskDto>> getFiltered(@PathVariable int priority) {
+    public ResponseEntity<List<TaskDto>> getFiltered(@PathVariable @Min(1) @Max(5) int priority) {
         List<TaskDto> dtos = TaskRepository.getRepository()
                 .stream()
                 .filter(task -> task.getPriority() == priority)
