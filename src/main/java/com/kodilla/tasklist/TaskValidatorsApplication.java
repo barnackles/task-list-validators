b561c9fbb7c1b5d1e68fa8cf9868831d434307a4
barnackles <p.martewicz@yahoo.com>
2023-05-29 17:20:01 +0200

    Project setup.

---
+++ src/main/java/com/kodilla/tasklist/TaskValidatorsApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskValidatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskValidatorsApplication.class, args);
+	}
+
+}
