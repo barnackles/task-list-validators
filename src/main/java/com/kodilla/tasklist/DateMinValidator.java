df52b7b45af2348e62ce42cbc51f79c9a193f287
barnackles <p.martewicz@yahoo.com>
2023-06-04 13:11:27 +0200

    Initialiaze method added in order to allow access to required value.

--- src/main/java/com/kodilla/tasklist/DateMinValidator.java
+++ src/main/java/com/kodilla/tasklist/DateMinValidator.java
@@ -4,11 +4,19 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DateMinValidator implements ConstraintValidator<DateMin, LocalDate> {
+    private String annotationValue;
+
+    public void initialize(DateMin constraintAnnotation) {
+        annotationValue = constraintAnnotation.value();
+    }
 
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
-        return false;
+        LocalDate minDate = LocalDate.parse(annotationValue, DateTimeFormatter.ISO_DATE);
+        return value.isAfter(minDate);
     }
+
 }
